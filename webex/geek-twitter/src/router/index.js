f6e955ec969f823f115272d6f9ba80f763ea1a09
樋口裕貴 <sai102343@gmail.com>
2021-05-28 22:05:48 +0900

    vue-routerを導入

---
+++ webex/geek-twitter/src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from "vue"
+import VueRouter from "vue-router"
+import Home from "../views/Home.vue"
+import About from "../views/About.vue"
+import MyPage from "../views/MyPage.vue"
+
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+  {
+    path: "/my-page",
+    name: "MyPage",
+    component: About,
+  },
+  {
+    path: "/my-page",
+    name: "MyPage",
+    component: MyPage, // これを更新
+  },
+]
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+})
+
+export default router
